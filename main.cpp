eb572dac0f8f40430c064e1cd96095e48e60f371
Luis Cardenas Peredo <19116@portalcedi.edu.mx>
2024-03-19 13:38:22 -0600

    Aña

--- main.cpp
+++ main.cpp
@@ -3,5 +3,25 @@ using namespace std;
 
 int main(){
 
+   class RedSocial{
+    private:
+    vector <Usuario> usuarios;
+    vector <Publicacion> publicaciones;
+
+    public:
+    string nombre;
+    int numeroDeUsuarios;
+    int numeroDePublicaciones;
+    
+    RedSocial(); 
+    RedSocial(string nom, int nu, int np);
+    
+    void agregarUsuario();
+    void mostrarUsuario();
+    void mostrarPublicacion();
+    Usuario* getUsuario (int id);
+
+};
+
   return 0;
 }
